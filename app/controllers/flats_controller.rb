7ad26ea7c63747b807f81382a04cf4f66559fe7b
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-13 10:58:58 +0200

    all functions are good, missing finaly position relative

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -14,7 +14,7 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     @flat.save
-    if @flat.save
+    if @flat.save!
       redirect_to flat_path(@flat)
     else
       render :new
@@ -40,7 +40,7 @@ class FlatsController < ApplicationController
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :image_url)
   end
 
 end
